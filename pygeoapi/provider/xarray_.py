c85680a9222eca9cbe2108ad4ab2daf95c5bc969
Piotr Zaborowski <pzaborowski@ogc.org>
2023-08-16 14:50:02 +0200

    Change order of y coordinates according to the standard (from min to max). Support by other than y,x,t order of dimension in the variables, on the cost of need to transpose - tbc if the non-needed transposition is costly.

--- pygeoapi/provider/xarray_.py
+++ pygeoapi/provider/xarray_.py
@@ -348,8 +348,8 @@ class XarrayProvider(BaseProvider):
                         'num': metadata['width']
                     },
                     'y': {
-                        'start': maxy,
-                        'stop': miny,
+                        'start': miny,
+                        'stop': maxy,
                         'num': metadata['height']
                     },
                     self.time_api_label: {
@@ -405,7 +405,7 @@ class XarrayProvider(BaseProvider):
                 }
 
                 data = data.fillna(None)
-                cj['ranges'][key]['values'] = data[key].values.flatten().tolist()  # noqa
+                cj['ranges'][key]['values'] = data[key].transpose(self.y_field,self.x_field,self.time_field).values.flatten().tolist()  # noqa
         except IndexError as err:
             LOGGER.warning(err)
             raise ProviderQueryError('Invalid query parameter')
